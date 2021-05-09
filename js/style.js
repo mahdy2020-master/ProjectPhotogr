668a21d1bcc3616c55d8676559330d0c531450ad
mahdy <ahmed43sam3030@gmail.com>
2021-05-09 07:37:30 +0200

    thetst

---
+++ js/style.js
@@ -0,0 +1,6 @@
+let a2=document.getElementById("the-a-2");
+let iteam2=document.getElementById("iteam-2");
+
+a2.addEventListener("click",function(){
+    iteam2.setAttribute("class"," iteam-2 d-flex justify-content-center align-items-center  animate__animated animate__bounceInDown ")
+})
